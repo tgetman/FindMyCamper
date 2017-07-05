c6c470c968602a36e6ff2c1a9b0a096462973cb6
tgetman <tjg01@me.com>
2017-07-05 17:44:08 -0400

    more vars

--- findmycamper.py
+++ findmycamper.py
@@ -21,20 +21,30 @@ CF.Key.set(KEY)
 
 # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
 img_url1 = 'http://worldversus.com/img/george-clooney.jpg'
-img_url2 = 'http://i1.wp.com/peopledotcom.files.wordpress.com/2017/05/george-clooney5.jpg?crop=0px%2C0px%2C1454px%2C970.66483516484px&resize=728%2C486&ssl=1'
+img_url2 = 'https://thumbs.dreamstime.com/z/two-women-hugging-smiling-15631732.jpg'
 
 detect1 = CF.face.detect(img_url1)
 print detect1
-
+faceId1 = detect1[0]['faceId']
+print faceId1
 
 detect2 = CF.face.detect(img_url2)
 print detect2
+faceId2 = detect2[0]['faceId']
+print faceId2
+faceId3 = detect2[1]['faceId']
+print faceId3
 
+result1 = CF.face.verify(faceId1 , faceId2 )
+result2 = CF.face.verify(faceId1, faceId3 )
 
-result1 = CF.face.verify('41bf98c3-4857-4dfe-a9d2-9f2d3f2049f9' , '858adce5-b94d-4e36-828b-d75b636f0eea' )
-result2 = CF.face.verify('41bf98c3-4857-4dfe-a9d2-9f2d3f2049f9', '64ddbc97-7496-480d-8e1d-f93a25f8933f' )
-result3 = CF.face.verify('41bf98c3-4857-4dfe-a9d2-9f2d3f2049f9', '41bf98c3-4857-4dfe-a9d2-9f2d3f2049f9')
 
 print result1
 print result2
-print result3
+
+if result1['isIdentical']== 'True':
+	print "face 1 and 2"
+if result2['isIdentical']== 'True':
+	print "face 1 and 3"
+
+
